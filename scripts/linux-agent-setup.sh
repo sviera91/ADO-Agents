1f70640bfa827264f877350722493b452491ab91
Stewart Viera <stewart.viera@gmail.com>
2020-05-18 23:36:25 -0400

    fixes

--- scripts/linux-agent-setup.sh
+++ scripts/linux-agent-setup.sh
@@ -64,7 +64,7 @@ sudo mv ./terraform /usr/local/bin/terraform
 
 echo "--- Setting Azure DevOps Agent ---"
 wget https://vstsagentpackage.azureedge.net/agent/2.168.2/vsts-agent-linux-x64-2.168.2.tar.gz
-sudo mkdir ado-agent && sudo chmod o+w ado-agent && cd ado-agent
+sudo mkdir ado-agent && sudo chmod o+w ado-agent/ && sudo chown $USER ado-agent/ && cd ado-agent
 sudo tar zxvf /home/$USER/vsts-agent-linux-x64-2.168.2.tar.gz
 ./config.sh --unattended --url $URL --auth pat --token $PAT --pool $POOL --agent $AGENT --acceptTeeEula
 ./svc.sh install
