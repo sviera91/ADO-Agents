1b723b274b816642f84bad27d0fc389a40fe5254
Stewart Viera <16260019+sviera91@users.noreply.github.com>
2021-03-09 02:41:57 -0500

    switching apt to apt-get

--- scripts/linux-agent-setup.sh
+++ scripts/linux-agent-setup.sh
@@ -10,24 +10,24 @@ USER=$5
 
 echo "Updating packages ..."
 cd /home/$USER
-apt update
-apt upgrade -y
+apt-get update
+apt-get upgrade -y
 
 echo "--- Update Python ---"
 sudo apt-get upgrade -y python3
-sudo apt install -y python3-pip
+sudo apt-get install -y python3-pip
 
 echo "--- Installing Azure CLI ---"
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 echo "--- Installing AWS CLI ---"
-sudo apt install unzip
+sudo apt-get install unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 
 echo "--- Installing Docker ---"
-sudo apt install -y \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -42,12 +42,12 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 #sudo usermod -aG docker $USER
 
 echo "--- Installing kubectl ---"
-sudo apt update && sudo apt install -y apt-transport-https gnupg2
+sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
